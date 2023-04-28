e3fa38625d63ddbce02f60fa9ad8d5e5a703e557
chuans <juaazn.05@gmail.com>
2023-04-27 19:50:31 -0500

    Route relatos de la comunidad naranja

---
+++ src/app/relatos-de-la-comunidad/verde/page.jsx
@@ -0,0 +1,15 @@
+import LayoutContent from '@/components/LayoutContent'
+import Title from '@/components/Title'
+import CardInfoUser from '@/components/CardInfoUser'
+import BarNavegation from '@/components/BarNavegation'
+import StoryTitle from '@/components/StoryTitle'
+
+export default function Verde () {
+  return (<LayoutContent>
+    <Title title='RELATOS DE LA COMUNIDAD'/>
+    <BarNavegation />
+    <StoryTitle name='Verde' nameColor='green' svgName='verde'/>
+    <CardInfoUser name='DAVID, 25 AÑOS' butt={false} />
+    <CardInfoUser name='SARA, 20 AÑOS' imageLeft={false} left={false} butt={false}/>
+  </LayoutContent>)
+}

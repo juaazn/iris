9a246d19d238df29d23e38ad483b12eded2c4615
chuans <juaazn.05@gmail.com>
2023-04-27 19:22:44 -0500

    Remove component video

--- src/app/historias/david/page.jsx
+++ src/app/historias/david/page.jsx
@@ -1,14 +1,12 @@
 import LayoutContent from '@/components/LayoutContent'
 import CardInfoUser from '@/components/CardInfoUser'
 import Title from '@/components/Title'
-import Video from '@/components/Videos'
 
 export default function David () {
   return (
     <LayoutContent>
       <Title seeText={false} title='DAVID, 25 AÑOS' imgName='image-user'/>
       <CardInfoUser name='DAVID, 25 AÑOS' butt={false} imgName='img-carta-david'/>
-      <Video videoName='draw-my-life-hugo.mp4'/>
     </LayoutContent>
   )
 }

1771ae315750ff71b8a4c38f0b28ca19563bef10
chuans <juaazn.05@gmail.com>
2023-04-27 23:03:35 -0500

    Added content

--- src/app/podcast/page.jsx
+++ src/app/podcast/page.jsx
@@ -6,7 +6,10 @@ import Title from '@/components/Title'
 export default function Podcast () {
   return (
     <LayoutContent imgBackground='background-podcast'>
-      <Title title='PODCAST "El CLUB DE LECTURA"' imgName='podcast-img'/>
+      <Title
+      title='PODCAST "El CLUB DE LECTURA"'
+      imgName='podcast-img'
+      titletex='"¡Bienvenidos al podcast de Iris! Aquí, nos adentramos en las emocionantes historias de la comunidad LGBTIQ+ mientras exploramos las experiencias, los desafíos y los triunfos que se encuentran entre las páginas del libro."'/>
       <KnowMore name='¿QUIRES ESCUCHARNOS?'/>
       <Button name='Haz Click aquí'/>
     </LayoutContent>

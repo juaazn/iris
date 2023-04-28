df44d296dbc84070f7dfe87197b5c90f7bdf4bb7
chuans <juaazn.05@gmail.com>
2023-04-28 09:50:03 -0500

    link added in button for wiki

--- src/app/diccionario/page.jsx
+++ src/app/diccionario/page.jsx
@@ -11,7 +11,8 @@ export default function Diccionario () {
       imgName='libro-iris'
       titletex='Te damos la más cordial bienvenida a nuestro Wiki diccionario, un lugar donde podrás sumergirte en el fascinante mundo de la comunidad LGBTIQA+. Aquí encontrarás una amplia gama de términos que te permitirán comprender mejor este universo diverso y en constante evolución. Explora el origen y significado de cada palabra, y siéntete libre de expresar tus opiniones sobre cómo se utilizan en el día a día. Además, este es un espacio abierto para compartir lo que estas palabras representan para ti.'/>
       <KnowMore name='¿QUIRES SABER MÁS?'/>
-      <Button name='Haz Click aquí'/>
+      <Button name='Haz Click aquí'
+      page='https://sites.google.com/view/glosario-de-iris/iris-home/términos/orientación-sexual'/>
     </LayoutContent>
   )
 }

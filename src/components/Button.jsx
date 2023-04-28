4ecc7b8d0937c01868195dc7c6ea7ec7b9ca7963
chuans <juaazn.05@gmail.com>
2023-04-27 20:02:22 -0500

    New feature link button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
 import styles from '../styles/Button.module.css'
 
-export default function Button ({ name = 'Botón' }) {
+import Link from 'next/link'
+
+export default function Button ({ name = 'Botón', page }) {
   return (
-    <a className={styles.mainButton} href='#'>{name}</a>
+    <Link className={styles.mainButton} href={`/${page}`}>{name}</Link>
   )
 }

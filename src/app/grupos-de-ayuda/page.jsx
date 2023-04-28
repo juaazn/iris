30fda2374eccd2ed2a1a9326b821eb4a09f28387
chuans <juaazn.05@gmail.com>
2023-04-27 19:49:01 -0500

    Route grupos de ayuda

---
+++ src/app/grupos-de-ayuda/page.jsx
@@ -0,0 +1,19 @@
+import LayoutContent from '@/components/LayoutContent'
+import Title from '@/components/Title'
+
+export default function GruposDeApoyo () {
+  return (
+    <LayoutContent>
+      <Title title="GRUPOS DE APOYO"/>
+      <div className="fb-page" data-href="https://www.facebook.com/profile.php?id=100091316603412" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/profile.php?id=100091316603412" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/profile.php?id=100091316603412">Proyecto Iris</a></blockquote></div>
+
+      <div id="fb-root"></div>
+      <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v16.0" nonce="TZmGZU8B"></script>
+      <hr />
+      <iframe
+        src='https://video.eko.com/preview/Vo1O6x?url=%2F%2Fstage.eko.com%2Fprojects%2FMQXBNN%2Ffc42edc1-e227-11ed-8d04-f19f01e44f76%2Farchive%2Fpreview%2F1682303419519%2Fembed.html&APIVersion=4.0.0&autoplay=true'>
+    </iframe>
+    <hr />
+    <iframe src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1498950139&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>    </LayoutContent>
+  )
+}

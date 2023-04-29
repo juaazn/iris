aaf6d50bc71ae43e66ffc85850a5416896bbbea6
chuans <juaazn.05@gmail.com>
2023-04-29 16:29:23 -0500

    Correction of link component in the top

--- src/app/grupos-de-ayuda/page.jsx
+++ src/app/grupos-de-ayuda/page.jsx
@@ -7,15 +7,18 @@ export default function GruposDeApoyo () {
     <LayoutContent>
       <Title title="GRUPOS DE APOYO"/>
       <TextGruposAyuda
+      img='colombia-diversa'
       texto='Colombia Diversa: Es una organización sin fines de lucro que trabaja por la igualdad de derechos de la comunidad LGBTIQ+ en Colombia, promoviendo la participación ciudadana y el diálogo con el Estado para garantizar la protección de los derechos de la comunidad.'
       nameLink='Visita Colombia Diversa'
       link='https://colombiadiversa.org
       '/>
       <TextGruposAyuda
+      img='caribe-afirmativo'
       texto='Caribe Afirmativo: Es una organización que trabaja por la defensa de los derechos humanos de la comunidad LGBTIQ+ en la región caribeña de Colombia, promoviendo la inclusión y la igualdad de oportunidades para todos.'
       nameLink='Visita Caribe Afirmativo'
       link='https://caribeafirmativo.lgbt'/>
       <TextGruposAyuda
+      img='santamaria-fundacion'
       texto='Santamaría Fundación: Es una organización sin fines de lucro que trabaja por el desarrollo y el bienestar de la comunidad LGBTIQ+ en Colombia, promoviendo la educación, el diálogo y la inclusión social.
       '
       nameLink=' Visita Santamaría
@@ -23,23 +26,28 @@ export default function GruposDeApoyo () {
       link='https://www.sfcolombia.org
       '/>
       <TextGruposAyuda
+      img='fundacio-diversas'
       texto='Casa Diversa: Es una organización que brinda atención psicológica y asesoría jurídica a la comunidad LGBTIQ+ en Colombia, promoviendo la salud mental y el bienestar emocional de sus usuarios.'
       nameLink='Visita Casa Diversa Comuna 8'
       link='https://www.facebook.com/casadiversac8/
       '/>
       <TextGruposAyuda
+      img='casa-colibri'
       texto='Colibríes: Es una organización que trabaja por la inclusión y la defensa de los derechos de la comunidad trans en Colombia, promoviendo la igualdad y la visibilidad de las personas trans en la sociedad.'
       nameLink='Visita Colibríes'
       link='https://mujeresalborde.org/programa/casa-colibri/'/>
       <TextGruposAyuda
+      img='red-comunitaria-trans'
       texto='Red Comunitaria Trans: Es una red de organizaciones y activistas que trabajan por los derechos de las personas trans en Colombia, promoviendo la inclusión y la igualdad de oportunidades para todos.'
       nameLink='Visita Red Comunitaria Trans'
       link='https://redcomunitariatrans.org'/>
       <TextGruposAyuda
+      img='fundacio-diversas'
       texto='Familias por la Diversidad: Es una organización sin fines de lucro que trabaja por la inclusión y el respeto de la diversidad sexual y de género en las familias, ofreciendo apoyo y orientación a padres y madres de hijos LGBTIQ+.'
       nameLink='Visita Familias por la Diversidad'
       link='https://familiasporladiversidad.org'/>
       <TextGruposAyuda
+      img='fundacion-familia'
       texto='Fundación En Familia: Es una organización que trabaja por la inclusión y el respeto de la diversidad sexual y de género en las familias, brindando apoyo, orientación y recursos a padres y madres de hijos LGBTIQ+.'
       nameLink='Visita Fundación En Familia'
       link='https://www.grupofamilia.com/fundacion-grupo-familia/'/>
--- src/components/TextGruposAyuda.jsx
+++ src/components/TextGruposAyuda.jsx
@@ -5,15 +5,13 @@ import LayoutContent from '@/components/LayoutContent'
 export default function TextGruposAyuda ({ texto, link, nameLink, img }) {
   return (
     <LayoutContent>
-      <div>
         <ul className={styles.infoContainer}>
           <li className={styles.infoContainer__flex}>
-            <p className={styles.text}>{texto}
             <a className={styles.link} href={link} target='_blank' rel='noreferrer'>{nameLink}</a>
+            <p className={styles.text}>{texto}
             </p>
           </li>
         </ul>
-      </div>
     </LayoutContent>
   )
 }

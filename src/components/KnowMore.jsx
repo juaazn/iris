888ea07772c8395ae85fff721cba24998fd53840
chuans <juaazn.05@gmail.com>
2023-05-03 10:32:47 -0500

    new update feautere

--- src/components/KnowMore.jsx
+++ src/components/KnowMore.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/KnowMore.module.css'
 
-export default function KnowMore ({ name }) {
+export default function KnowMore ({ name, left = false }) {
   return (
-    <h4 className={styles.knowMore}>{name}</h4>
+    <h4 className={left ? styles.knowMore__left : styles.knowMore}>{name}</h4>
   )
 }

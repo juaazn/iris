a7c8825b4c7a871d5d64bd14e2623b7c6266add8
chuans <juaazn.05@gmail.com>
2023-05-03 10:42:04 -0500

    New images for storys purple

--- src/app/relatos-de-la-comunidad/morado/page.jsx
+++ src/app/relatos-de-la-comunidad/morado/page.jsx
@@ -1,19 +1,20 @@
 import LayoutContent from '@/components/LayoutContent'
 import Title from '@/components/Title'
-import CardInfoUser from '@/components/CardInfoUser'
 import BarNavegation from '@/components/BarNavegation'
 import StoryTitle from '@/components/StoryTitle'
+import KnowMore from '@/components/KnowMore'
+import StorysImages from '@/components/StorysImages'
 
-export default function Verde () {
+export default function Morado () {
   return (
     <LayoutContent>
       <Title title='RELATOS DE LA COMUNIDAD'/>
       <BarNavegation />
       <StoryTitle name='Morado' nameColor='purple' svgName='morado'/>
-      <CardInfoUser
-      name='VALERY, 20 AÑOS'
-      butt={false}
-      texto='Desde hace un año, mantengo una relación a distancia con una chica de España. Ella marcó mi vida en el sentido de que me ayudo a aceptarme a mí misma cuando la bisexualidad era un tema difícil para mí, y me hizo sentir amada de una manera que nadie lo había hecho. Una de mis metas en la vida, algo que me motiva a levantarme por las mañanas y seguir adelante, es cerrar la distancia física que existe entre nosotras.'/>
+      <KnowMore
+      name='SANDRA, 19 AÑOS'
+      left={true}/>
+      <StorysImages images='historias-morado-1'/>
     </LayoutContent>
   )
 }

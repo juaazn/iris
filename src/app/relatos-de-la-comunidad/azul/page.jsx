eafb3be87297e222f2c4b608f15a0d6c70a3a80b
chuans <juaazn.05@gmail.com>
2023-05-03 10:41:47 -0500

    New images for storys blue

--- src/app/relatos-de-la-comunidad/azul/page.jsx
+++ src/app/relatos-de-la-comunidad/azul/page.jsx
@@ -1,19 +1,20 @@
 import LayoutContent from '@/components/LayoutContent'
 import Title from '@/components/Title'
-import CardInfoUser from '@/components/CardInfoUser'
 import BarNavegation from '@/components/BarNavegation'
 import StoryTitle from '@/components/StoryTitle'
+import KnowMore from '@/components/KnowMore'
+import StorysImages from '@/components/StorysImages'
 
-export default function Verde () {
+export default function Azul () {
   return (
     <LayoutContent>
       <Title title='RELATOS DE LA COMUNIDAD'/>
       <BarNavegation />
       <StoryTitle name='Azul' nameColor='blue' svgName='azul'/>
-      <CardInfoUser
-      name='MIGUEL, 23 AÑOS'
-      butt={false}
-      texto='En el colegio tuve la oportunidad de formar una amistad con un amigo que ahora se reconoce como mujer, viví y evidencie el proceso por el cual paso, desde sufrir de comentarios negativos por sus preferencias o forma de activar, hasta ver el rechazo de los demás. A pesar de todo estuve allí, y ahora ver su proceso y la nueva decisión que ha tomado me llena de orgullo y felicidad.'/>
+      <KnowMore
+      name='CAROLINA, 20 AÑOS'
+      left={true}/>
+      <StorysImages images='historias-azul-1'/>
     </LayoutContent>
   )
 }

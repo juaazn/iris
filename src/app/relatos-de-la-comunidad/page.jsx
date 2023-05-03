ba56a6cb01a54f8413fa3d06e391c8205a8102ce
chuans <juaazn.05@gmail.com>
2023-05-03 10:40:05 -0500

    Added images to storys red

--- src/app/relatos-de-la-comunidad/page.jsx
+++ src/app/relatos-de-la-comunidad/page.jsx
@@ -1,8 +1,9 @@
 import LayoutContent from '@/components/LayoutContent'
 import Title from '@/components/Title'
-import CardInfoUser from '@/components/CardInfoUser'
 import BarNavegation from '@/components/BarNavegation'
 import StoryTitle from '@/components/StoryTitle'
+import StorysImages from '@/components/StorysImages'
+import KnowMore from '@/components/KnowMore'
 
 export default function RelatosDeLaComunidad () {
   return (
@@ -10,11 +11,10 @@ export default function RelatosDeLaComunidad () {
       <Title title='RELATOS DE LA COMUNIDAD'/>
       <BarNavegation color={true}/>
       <StoryTitle name='Rojo'svgName='rojo' nameColor='#FF0000'/>
-      <CardInfoUser
-      name='Luisa, 20 AÑOS'
-      imgName='user-sara'
-      butt={false}
-      texto='Hace siete años conocí que era una amar a una persona por su alma y no por su identidad de género, a pesar de los problemas y estigmas de las demás personas. Años después pude estar con esa persona hasta el día de hoy llenándome de orgullo poder amar sin ser juzgada.'/>
+      <KnowMore
+      name='EMILY, 21 AÑOS'
+      left={true}/>
+      <StorysImages images='histoias-rojo-1'/>
     </LayoutContent>
   )
 }

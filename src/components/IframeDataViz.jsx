a789a8ae4ae689b21fa74362ed9148e5168f6c76
chuans <juaazn.05@gmail.com>
2023-05-05 08:38:57 -0500

    Added Attribute loading eager

--- src/components/IframeDataViz.jsx
+++ src/components/IframeDataViz.jsx
@@ -3,7 +3,7 @@ import styles from '@/styles/IframeDataViz.module.css'
 export default function IframeDataViz ({ urlDataViz }) {
   return (
     <>
-      <iframe className={styles.iframe} title='DATA VIZ 2' src={`${urlDataViz}`} type='text/html' allowfullscreen='true'></iframe>
+      <iframe className={styles.iframe} title='DATA VIZ 2' src={`${urlDataViz}`} type='text/html' allowfullscreen='true' loading='eager'></iframe>
     </>
   )
 }
